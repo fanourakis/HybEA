9fc1621822bc67eed10947a62fc5a7c92a6fbe5c
Nikolaos Fanourakis <92919367+fanourakis@users.noreply.github.com>
2024-10-09 19:05:59 +0300

    Update Param.py

--- src/Param.py
+++ src/Param.py
@@ -1,4 +1,4 @@
-# Choose between Hybea, Hybea_struct_first, Hybea_without_structure, Hybea_without_factual, Hybea_basic, Hybea_basic_structure_first
+# Choose between Hybea, Hybea_light Hybea_struct_first, Hybea_without_structure, Hybea_without_factual, Hybea_basic, Hybea_basic_structure_first
 MODE = "Hybea"
-# Choose between D_W_15K_V1, D_W_15K_V2, SRPRS_D_W_15K_V1, SRPRS_D_W_15K_V2, BBC_DB
+# Choose between D_W_15K_V1, D_W_15K_V2, SRPRS_D_W_15K_V1, SRPRS_D_W_15K_V2, BBC_DB, fr_en, ja_en, zh_en
 DATA = "D_W_15K_V2"
